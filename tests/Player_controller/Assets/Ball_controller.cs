c93103c499ea55023b9073ccfc6ba56fce1f9722
itectori <mounie_c@epita.fr>
2016-02-18 21:28:03 +0100

    Revert "Tout va bien tkt"
    This reverts commit 77ecbd68756d4da7f3c17c6d41dce703f2922b59.

---
+++ tests/Player_controller/Assets/Ball_controller.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Assets
+{
+    public class Ball_controller : MonoBehaviour
+    {
+        private GameObject passeur; //joueur possédant la balle juste avant la passe
+        private Vector3 arrivalPoint; // point d'arrivée de la passe
+        private bool deplacement; // Balle en l'air
+
+        void Start()
+        {
+            deplacement = false;
+			passeur = transform.parent.gameObject;
+        }
+
+        void Update()
+        {
+            if (!deplacement)
+            {
+                if (Input.GetKeyDown(KeyCode.A) && transform.parent != null)
+                {
+                    deplacement = transform.parent.GetComponent<Player_controller>().passe(ref arrivalPoint); //Renvoit true si la passe est possible
+                    if (deplacement)//debut de la passe
+                    {
+                        passeur = transform.parent.gameObject;
+                        transform.parent = null; // Detache la balle du joueur
+                    }
+                }
+            }
+            else
+            {
+                if (transform.position == arrivalPoint || transform.parent != null)// fin de la passe ou interception
+                {
+                    deplacement = false;
+                }
+                if (deplacement)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, arrivalPoint, Time.deltaTime * 20);// mouvement de la passe
+                }
+            }
+
+
+        }
+        public bool interceptable(GameObject player) // evite le passeur d'intercepter sa propre balle, renvoit false si l'intercepteur est le lanceur
+        {
+            return player != passeur || !deplacement;
+        }
+
+		void OnTriggerEnter(Collider other)
+		{
+			GameObject gmCol = other.gameObject; // gm touche
+			/*if (gmCol.CompareTag("Player"))
+			{
+				passed = false;
+				holder = gmCol; // devient le parent
+				gameObject.transform.SetParent(holder.transform);
+				transform.localPosition = new Vector3(0, 0.8f,0); // se met sur lui
+			}else */
+			if (gmCol.CompareTag("Goal") && passeur != null)
+			{
+				GoalController g_controller = gmCol.GetComponent<GoalController> ();
+				g_controller.goal ();
+			}
+		}
+    }
+}
---
+++ tests/Player_controller/Assets/Game.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game {
+
+	private static Game game_instance = new Game ();
+	public static Game Instance {
+		get{
+			return game_instance;
+		}
+	}
+
+	private Team[] teams;
+	public Team[] Teams {
+		get {
+			return teams;
+		}
+	}
+
+	public Game (){
+		teams = new Team[2];
+		teams [0] = new Team ("SuperTeam1");
+		teams [1] = new Team ("SuperTeamMega2");
+	}
+
+	public void goal(int team_id){
+		Team team = teams [team_id];
+		team.Points = 1;
+		//Debug.Log (team.Name + " :"+ team.Points);
+	}
+}
