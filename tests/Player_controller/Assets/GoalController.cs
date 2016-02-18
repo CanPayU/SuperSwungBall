a57deff30cc0b5acb034258069a4c4f3131d9fdb
Lombrix <romain.foucher@epita.fr>
2016-02-18 20:53:22 +0100

    Rien
    Rien

--- tests/Player_controller/Assets/GoalController.cs
+++ tests/Player_controller/Assets/GoalController.cs
@@ -1,26 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GoalController : MonoBehaviour {
+public class GoalController : MonoBehaviour
+{
 
-	[SerializeField]
-	private int team_id;
+    [SerializeField]
+    private int team_id;
 
-	private GameObject main;
+    private GameObject main;
 
-	// Use this for initialization
-	void Start () {
-		main = GameObject.Find ("Main");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        main = GameObject.Find("Main");
+    }
 
-	public void goal(){
-		Game.Instance.goal (team_id);
-		MainController controller = main.GetComponent<MainController> ();
-		controller.update_score ();
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void goal()
+    {
+        Game.Instance.goal(team_id);
+        MainController controller = main.GetComponent<MainController>();
+        controller.update_score();
+    }
 }
