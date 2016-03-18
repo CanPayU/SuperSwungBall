b3a92f69fa2c166f594b6b46092190fdce04df20
Lombrix <romain.foucher@epita.fr>
2016-03-18 01:41:11 +0100

    Didacticiel v0.1 + ctrl K+D
    Affiche le début du texte dans le didacticiel
    
    Petite correction dans le choiceController
    
    ctrl K+D dans le script game, dans le CollisionController du multi et
    dans le GoalController du game,

--- SuperSwungBall_f/Assets/Script/Controller/Game/GoalController.cs
+++ SuperSwungBall_f/Assets/Script/Controller/Game/GoalController.cs
@@ -8,13 +8,15 @@ namespace GameScene
     public class GoalController : MonoBehaviour
     {
 
-		[SerializeField]
-		private int team_id;
-		public int Team {
-			set {
-				team_id = value;
-			}
-		}
+        [SerializeField]
+        private int team_id;
+        public int Team
+        {
+            set
+            {
+                team_id = value;
+            }
+        }
 
         private GameObject main;
 
@@ -26,28 +28,31 @@ namespace GameScene
 
         public void goal()
         {
-            
 
-			Debug.Log("GOAAL RPC");
-			if (PhotonNetwork.inRoom) {
-				PhotonView pv = PhotonView.Get (this); // Impossible car GoalController n'a pas de PhotonView
-				Debug.Log (pv);
-				Debug.Log (team_id);
-				pv.RPC ("Goal_RPC", PhotonTargets.All, team_id);
-			} else {
-				Game.Instance.goal(team_id);
-				main.GetComponent<Main_Controller>().update_score();
-			}
+
+            Debug.Log("GOAAL RPC");
+            if (PhotonNetwork.inRoom)
+            {
+                PhotonView pv = PhotonView.Get(this); // Impossible car GoalController n'a pas de PhotonView
+                Debug.Log(pv);
+                Debug.Log(team_id);
+                pv.RPC("Goal_RPC", PhotonTargets.All, team_id);
+            }
+            else {
+                Game.Instance.goal(team_id);
+                main.GetComponent<Main_Controller>().update_score();
+            }
 
 
         }
 
-		[PunRPC]private void Goal_RPC(int teamID) //event collison
-		{
-			Debug.Log("GOAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAL");
-			Game.Instance.goal(team_id);
-			main.GetComponent<MainController>().update_score();
-		}
+        [PunRPC]
+        private void Goal_RPC(int teamID) //event collison
+        {
+            Debug.Log("GOAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAL");
+            Game.Instance.goal(team_id);
+            main.GetComponent<MainController>().update_score();
+        }
     }
 }
 
