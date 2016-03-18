c13d755dd2c7c893969ca68df4c4322207d66b32
Lombrix <romain.foucher@epita.fr>
2016-03-18 16:23:30 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	SuperSwungBall_f/Assets/Scene/Didacticiel.unity
    
    #	SuperSwungBall_f/Assets/Script/Controller/Game/Didacticiel/MainController.cs
    
    #	SuperSwungBall_f/Assets/Script/Controller/Game/Didacticiel/MenuController.cs

---
+++ SuperSwungBall_f/Assets/Script/Controller/Game/Didacticiel/MainController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+
+namespace Didacticiel
+{
+    public class MainController : MonoBehaviour
+    {
+        [SerializeField]
+        private Text screentext;
+        [SerializeField]
+        private GameObject player1_prefab;
+
+        /*   
+        private string screentext_text;
+        private float startTime;
+        */
+        private float current_time;
+        private string[,] tableau;
+        private int place;
+        int actions = 1;
+        Player player;
+
+        // Use this for initialization
+        void Start()
+        {
+            place = 0;
+            current_time = 0;
+            tableau = new string[,] {
+            {"Bienvenue dans le didactitiel","2" },
+            {"Comment jouer ?","3" },
+            {"Le but du jeu est de marquer 3 points", "3" },
+            {"Chaque joueur contrôle son équipe", "3" },
+            {"Commençons par bouger un Swungman", "3" }};
+            phrase(tableau[place, 0]);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (actions == 1)
+            {
+                if (current_time < time())
+                    current_time += Time.deltaTime;
+                else if (place == 4)
+                {
+                    ++actions;
+                    phrase();
+                }
+                else
+                {
+                    current_time = 0;
+                    ++place;
+                    phrase(tableau[place, 0]);
+                }
+            }
+
+            if (actions == 2)
+            {
+                player = new Player(10, 10, 10, 10, "Franck", 1);
+                GameObject play0 = Instantiate(player1_prefab, new Vector3(0, 1F, 0), Quaternion.identity) as GameObject;
+            }
+        }
+
+        void phrase(string s = "")
+        { screentext.text = s; }
+        float time()
+        { return float.Parse(tableau[place, 1]); }
+
+    }
+}
--- SuperSwungBall_f/Assets/Script/Player.cs
+++ SuperSwungBall_f/Assets/Script/Player.cs
@@ -7,32 +7,33 @@ using UnityEngine;
 [Serializable]
 public class Player
 {
-	private string player_name;
-	public string Name {
-		get { return player_name; }
-		set { player_name = value; }
-	}
-	private int team_id;
+    private string player_name;
+    public string Name
+    {
+        get { return player_name; }
+        set { player_name = value; }
+    }
+    private int team_id;
 
     private Dictionary<string, int> defaultStats = new Dictionary<string, int>(); // Stats initiales (unique au joueur)
-    private List<string> buttonsValues = new List<string> { "esquive","esquive","esquive" }; // Valeurs des boutons
+    private List<string> buttonsValues = new List<string> { "esquive", "esquive", "esquive" }; // Valeurs des boutons
     private Dictionary<string, int> finalStats = new Dictionary<string, int> { { "esquive", 0 }, { "tacle", 0 }, { "passe", 0 }, { "course", 0 } }; // Stats après selection des actions dans le menu
 
-	public Player(int tacle, int esquive, int passe, int course, string player_name_, int Team_Id)
+    public Player(int tacle, int esquive, int passe, int course, string player_name_, int Team_Id)
     {
         defaultStats.Add("esquive", esquive);
         defaultStats.Add("tacle", tacle);
         defaultStats.Add("passe", passe);
         defaultStats.Add("course", course);
         initialize_finaleStats();
-		player_name = player_name_;
-		team_id = Team_Id;
+        player_name = player_name_;
+        team_id = Team_Id;
     }
     private void initialize_finaleStats() // Initialises les stats finales
     {
         finalStats["tacle"] = 0;
         finalStats["esquive"] = 0;
-		finalStats["passe"] = 0;
+        finalStats["passe"] = 0;
         finalStats["course"] = defaultStats["course"] + 10;
     }
 
@@ -63,20 +64,20 @@ public class Player
     #region Getters
     public float Speed
     {
-		get { return (float)finalStats["course"] / 10; }
-	}
-	public float Esquive
-	{
-		get { return (float)finalStats["esquive"] / 10; }
-	}
-	public float Passe
-	{
-		get { return (float)finalStats["passe"] / 10; }
-	}
-	public float Tacle
-	{
-		get { return (float)finalStats["tacle"] / 10; }
-	}
+        get { return (float)finalStats["course"] / 10; }
+    }
+    public float Esquive
+    {
+        get { return (float)finalStats["esquive"] / 10; }
+    }
+    public float Passe
+    {
+        get { return (float)finalStats["passe"] / 10; }
+    }
+    public float Tacle
+    {
+        get { return (float)finalStats["tacle"] / 10; }
+    }
     public float ZoneDeplacement
     {
         get { return (float)finalStats["course"] / 10; }
@@ -84,15 +85,16 @@ public class Player
     public float ZonePasse
     {
         get { return (float)finalStats["passe"] / 10; }
-	}
-	public GameObject Gm
-	{
-		get { return GameObject.Find (player_name+"-"+team_id); }
-	}
-	public int Team_id {
-		get { return team_id; }
-		set { team_id = value; }
-	}
+    }
+    public GameObject Gm
+    {
+        get { return GameObject.Find(player_name + "-" + team_id); }
+    }
+    public int Team_id
+    {
+        get { return team_id; }
+        set { team_id = value; }
+    }
     #endregion
 }
 
