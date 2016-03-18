7c1e5132ba35d99657358d1a30d9568af91f5877
itectori <mounie_c@epita.fr>
2016-03-18 16:10:05 +0100

    Bouton Multi
    -Ajout animation Repos / Action
    -Ajout d'un "Animation Button Controller"

---
+++ SuperSwungBall_f/Assets/Script/Controller/Menu/AnimationButtonControler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationButtonControler : MonoBehaviour
+{
+
+    private Animator myAnimator;
+    void Start()
+    {
+        myAnimator = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    void OnMouseEnter()
+    {
+        myAnimator.Play("Action");
+    }
+
+    void OnMouseExit()
+    {
+        myAnimator.Play("Repos");
+    }
+
+}
