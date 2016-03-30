cded949ec4ef511da0b7b985f1ca4a32f1a46fbd
itectori <mounie_c@epita.fr>
2016-03-30 12:07:57 +0200

    Refonte des Collisions
    - Collisions en débuts de tour
    - changements des noms d'animations ( animations à faire )

--- SuperSwungBall_f/Assets/Script/Controller/Game/CollisionController.cs
+++ SuperSwungBall_f/Assets/Script/Controller/Game/CollisionController.cs
@@ -3,94 +3,128 @@ using System.Collections;
 
 namespace GameScene
 {
-	public class CollisionController : MonoBehaviour
-	{
-		Player player;
-		void Start()
-		{
-			player = GetComponent<PlayerController>().Player;
-		}
-		public void OnTriggerEnter(Collider other) //event collison
-		{
-			player = GetComponent<PlayerController>().Player;
-			if (GetComponent<PlayerController>().PhaseAnimation)
-			{
-				if (other.transform.parent == null && other.name == "Ball" && player.ZonePasse != 0 && other.GetComponent<BallController>().interceptable(gameObject)) // ramasse/intercepte la balle uniquement si le perso a au moins un élément "passe"
-				{
-					other.transform.parent = transform.FindChild("perso").transform;
-					other.GetComponent<Collider>().enabled = false;
-					other.transform.localPosition = new Vector3(1.3f, 3, 0);
-				}
+    public class CollisionController : MonoBehaviour
+    {
+        Player player;
+        bool goal;
+        int premiereFrames;
+        void Start()
+        {
+            player = GetComponent<PlayerController>().Player;
+            premiereFrames = 5;
+            goal = false;
+        }
 
-				if (other.tag == "Goal" && transform.FindChild("perso").transform.FindChild("Ball") != null) // GOAL
-				{
-					transform.FindChild("perso").GetComponent<Animator>().Play("TouchDown");
-					GetComponent<PlayerController>().Pause = 10f;
-					GoalController g_controller = other.GetComponent<GoalController>();
-					g_controller.goal();
-				}
+        public void OnTriggerStay(Collider other)
+        {
+            if (GetComponent<PlayerController>().PhaseAnimation)
+            {
+                if (premiereFrames > 0)
+                {
+                    interception(other);
+                    if (other.tag == "Player")
+                    {
+                        combat(other);
+                    }
+                    premiereFrames--;
+                    Debug.Log(other);
+                }
+                if (!goal)
+                {
+                    Goal(other);
+                }
+            }
+        }
+        public void OnTriggerEnter(Collider other)
+        {
+            player = GetComponent<PlayerController>().Player;
+            if (GetComponent<PlayerController>().PhaseAnimation)
+            {
+                interception(other);
+                if (other.tag == "Player")
+                {
+                    combat(other);
+                }
+            }
+        }
 
-				if (other.tag == "Player")
-				{
-					Player adversaire = other.GetComponent<PlayerController>().Player;
-					if (adversaire.Team_id != player.Team_id && (adversaire.Tacle != 0 || player.Tacle != 0)) // collision adversaire et déclechement combat
-					{
-						transform.FindChild("perso").transform.LookAt(new Vector3(other.transform.position.x, transform.FindChild("perso").position.y, other.transform.position.z)); // rotation des joueurs ( face à face)
+        private void interception(Collider ballCollider)
+        {
+            // ramasse/intercepte la balle uniquement si le perso a au moins un élément "passe"
+            if (ballCollider.transform.parent == null && ballCollider.name == "Ball" && player.ZonePasse != 0 && ballCollider.GetComponent<BallController>().interceptable(gameObject))
+            {
+                ballCollider.transform.parent = transform.FindChild("perso").transform;
+                ballCollider.GetComponent<Collider>().enabled = false;
+                ballCollider.transform.localPosition = new Vector3(1.3f, 3, 0);
+            }
+        }
+        private void Goal(Collider goalCollider)
+        {
+            // GOAL
+            if (goalCollider.tag == "Goal" && transform.FindChild("perso").transform.FindChild("Ball") != null)
+            {
+                transform.FindChild("perso").GetComponent<Animator>().Play("TouchDown");
+                GetComponent<PlayerController>().Pause = 10f;
+                GoalController g_controller = goalCollider.GetComponent<GoalController>();
+                g_controller.goal();
+                goal = true;
+            }
+        }
+        private void combat(Collider adversaireCollider)
+        {
+            Player adversaire = adversaireCollider.GetComponent<PlayerController>().Player;
+            // collision adversaire et déclenchement combat
+            if (adversaire.Team_id != player.Team_id && (adversaire.Tacle != 0 || player.Tacle != 0))
+            {
+                // rotation des joueurs ( face à face)
+                transform.FindChild("perso").transform.LookAt(new Vector3(adversaireCollider.transform.position.x, transform.FindChild("perso").position.y, adversaireCollider.transform.position.z));
 
-						int attaqueAdverse = (int)(Mathf.Max(adversaire.Tacle, adversaire.Esquive));
-						bool porteurDeBall = transform.FindChild("perso").transform.FindChild("Ball") != null;
-						if (player.Tacle > player.Esquive)
-						{
-							Debug.Log(player.Tacle);
-							if (player.Tacle > attaqueAdverse)
-							{
-								//animation Attaque
-								transform.FindChild("perso").GetComponent<Animator>().Play("Attaque");
-								GetComponent<PlayerController>().Pause = 1f;
-								Debug.Log(name + " réussit son tacle");
-							}
-							else
-							{
-								combatPerdu(porteurDeBall);
-								Debug.Log(name + " rate son tacle");
+                float attaqueAdverse = Mathf.Max(adversaire.Tacle, adversaire.Esquive);
+                bool porteurDeBall = transform.FindChild("perso").transform.FindChild("Ball") != null;
+                if (player.Tacle > player.Esquive)
+                {
+                    Debug.Log(player.Tacle);
+                    if (player.Tacle > attaqueAdverse)
+                    {
+                        //Attaque réussit
+                        transform.FindChild("perso").GetComponent<Animator>().Play("Attaque Reussit");
+                        GetComponent<PlayerController>().Pause = 1f;
+                        Debug.Log(name + " réussit son tacle");
+                    }
+                    else
+                    {
+                        //Attaque ratée
+                        transform.FindChild("perso").GetComponent<Animator>().Play("Attaque Echec");
+                        GetComponent<PlayerController>().Pause = 1f;
+                        Debug.Log(name + " rate son tacle");
 
-							}
-						}
-						else
-						{
-							Debug.Log(player.Esquive);
-							if (player.Esquive > attaqueAdverse)
-							{
+                    }
+                }
+                else
+                {
+                    Debug.Log(player.Esquive);
+                    if (player.Esquive > attaqueAdverse)
+                    {
+                        //Esquive Réussit
+                        transform.FindChild("perso").GetComponent<Animator>().Play("Esquive Reussit");
+                        GetComponent<PlayerController>().Pause = 2f;
+                        Debug.Log(name + " réussit son esquive");
+                    }
+                    else
+                    {
+                        //Esquive ratée
+                        transform.FindChild("perso").GetComponent<Animator>().Play("Esquive Echec");
+                        GetComponent<PlayerController>().Pause = 2f;
+                        Debug.Log(name + " rate son esquive");
+                    }
+                }
+            }
+        }
 
-								//transform.FindChild("perso").GetComponent<Animator>().Play("Esquive");
-								GetComponent<PlayerController>().Pause = 2f;
-								Debug.Log(name + " réussit son esquive");
-							}
-							else
-							{
-								combatPerdu(porteurDeBall);
-								Debug.Log(name + " rate son esquive");
-							}
-						}
-					}
-				}
-			}
-		}
-
-		private void combatPerdu(bool porteurDeBall)
-		{
-			//animation Chute
-			transform.FindChild("perso").GetComponent<Animator>().Play("Chute");
-			GetComponent<PlayerController>().Pause = 4f;
-
-			if (porteurDeBall)
-			{
-				GameObject ball = transform.FindChild("perso").transform.FindChild("Ball").gameObject;
-				Debug.Log(ball);
-				ball.transform.localPosition -= new Vector3(0, 2.4f, 5);
-				ball.GetComponent<Collider>().enabled = true;
-				ball.transform.parent = null;
-			}
-		}
-	}
+        public void Start_anim()
+        {
+            goal = false;
+            premiereFrames = 5;
+        }
+    }
 }
