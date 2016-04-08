80fe62fe03b9700af5ca11c2c8432c20f9135746
Lombrix <romain.foucher@epita.fr>
2016-04-08 19:41:49 +0200

    Casse les burnes
    oui

--- SuperSwungBall_f/Assets/Script/Controller/Game/Didactitiel/MainController.cs
+++ SuperSwungBall_f/Assets/Script/Controller/Game/Didactitiel/MainController.cs
@@ -4,15 +4,22 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using System;
 
-namespace Didactitiel
+namespace Didacticiel
 {
     public class MainController : MonoBehaviour
     {
         [SerializeField]
         private Text screentext;
+        [SerializeField]
+        private GameObject player1_prefab;
+        [SerializeField]
+        private GameObject player2_prefab;
 
-        /*   private string screentext_text;
-           private float startTime; */
+        /*   
+        private string screentext_text;
+        private float startTime; 
+        */
+        Timer time;
         private float current_time;
         private string[,] tableau;
         private int place;
@@ -27,14 +34,16 @@ namespace Didactitiel
             {"Comment jouer ?","3" },
             {"Le but du jeu est de marquer 3 points", "3" },
             {"Chaque joueur contrôle son équipe", "3" },
-            {"Commençons par bouger un Swungman", "3" }};
+            {"Commençons par bouger un Swungman", "3" } };
             screentext.text = phrase();
+            phase1();
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (current_time < time())
+            if ()
+            if (current_time < temps())
                 current_time += Time.deltaTime;
             else
             {
@@ -44,9 +53,26 @@ namespace Didactitiel
             }
         }
 
+        void phase1()
+        {
+
+        }
+        void phase2()
+        {
+            Team team_0 = Game.Instance.Teams[0];
+            Player play_t0 = Settings.Instance.Default_player["gpdn"];
+            bool isMine = (PhotonNetwork.isMasterClient);
+            GameObject play0 = Instantiate(player1_prefab, new Vector3(1F, 1F, 0F), Quaternion.identity) as GameObject;
+            play_t0.Team_id = 0;
+            play_t0.Name += "-" + 0;
+            play0.name = play_t0.Name + "-" + play_t0.Team_id;
+            PlayerController controller = play0.GetComponent<PlayerController>();
+            controller.Player = play_t0;
+            controller.IsMine = isMine;
+        }
         string phrase()
         { return screentext.text = tableau[place, 0]; }
-        float time()
+        float temps()
         { return float.Parse(tableau[place, 1]); }
 
     }
