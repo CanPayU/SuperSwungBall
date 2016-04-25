8da02dbfd30aee450512d39bbbdd6b53bf5c476b
Lombrix <romain.foucher@epita.fr>
2016-04-26 01:02:05 +0200

    Photon me les brise + indendation scripts standing
    Tout est dans le titre

--- SuperSwungBall_f/Assets/Script/Controller/Standing/ConnectionController.cs
+++ SuperSwungBall_f/Assets/Script/Controller/Standing/ConnectionController.cs
@@ -11,40 +11,43 @@ namespace Standing
         [SerializeField]
         private InputField password;
         [SerializeField]
-		private Button connect;
-		[SerializeField]
-		private Button withoutPass;
+        private Button connect;
+        [SerializeField]
+        private Button withoutPass;
 
-		private bool validated = false;
+        private bool validated = false;
 
         // Use this for initialization
         void Start()
         {
-			username.onValueChanged.AddListener(delegate{
-				OnChange(true);
-			});
-			password.onValueChanged.AddListener(delegate{
-				OnChange(false);
-			});
+            username.onValueChanged.AddListener(delegate
+            {
+                OnChange(true);
+            });
+            password.onValueChanged.AddListener(delegate
+            {
+                OnChange(false);
+            });
 
         }
 
-		private void OnChange(bool all){
-			if (this.validated) 
-			{
-				ColorBlock cb = connect.colors;
-				cb.normalColor = new Color (45f / 255f, 125f / 255f, 204f / 255f);
-				connect.colors = cb;
-				if (all)
-					withoutPass.colors = cb;
-				this.validated = false;
-			}
-		}
+        private void OnChange(bool all)
+        {
+            if (this.validated)
+            {
+                ColorBlock cb = connect.colors;
+                cb.normalColor = new Color(45f / 255f, 125f / 255f, 204f / 255f);
+                connect.colors = cb;
+                if (all)
+                    withoutPass.colors = cb;
+                this.validated = false;
+            }
+        }
 
         public void check_login()
         {
-			this.validated = true;
-			connect.interactable = false;
+            this.validated = true;
+            connect.interactable = false;
             HTTP.Authenticate(username.text, password.text, (success) =>
             {
                 if (success)
@@ -53,44 +56,48 @@ namespace Standing
                     FadingManager.I.Fade();
                 }
                 else
-				{
-					connect.interactable = true;
-					ColorBlock cb = connect.colors;
-					cb.normalColor = new Color(229f / 255f, 76f / 255f, 76f / 255f);
-					connect.colors = cb;
+                {
+                    connect.interactable = true;
+                    ColorBlock cb = connect.colors;
+                    cb.normalColor = new Color(229f / 255f, 76f / 255f, 76f / 255f);
+                    connect.colors = cb;
                 }
             });
         }
 
-		public void OnConnectWithDevice(){
-			withoutPass.interactable = false;
-			HTTP.AuthDeviceAsk (username.text, (success_ask) => {
-				if (success_ask)
-					Notification.Create(NotificationType.Text, "Code de confirmation", 0, "Entrez le code recus sur votre mobile", force: true, completion: (value, text) => {
-						HTTP.AuthDeviceReply(username.text, text, (success_reply) => {
-							if (success_reply)
-							{
-								SaveLoad.save_user();
-								FadingManager.I.Fade();
-							}
-							else
-							{
-								withoutPass.interactable = true;
-								ColorBlock cb = connect.colors;
-								cb.normalColor = new Color(229f / 255f, 76f / 255f, 76f / 255f);
-								withoutPass.colors = cb;
-							}
-						});
-					});
-				else 
-				{
-					withoutPass.interactable = true;
-					ColorBlock cb = connect.colors;
-					cb.normalColor = new Color(229f / 255f, 76f / 255f, 76f / 255f);
-					withoutPass.colors = cb;
-				}
-			});
-		}
+        public void OnConnectWithDevice()
+        {
+            withoutPass.interactable = false;
+            HTTP.AuthDeviceAsk(username.text, (success_ask) =>
+            {
+                if (success_ask)
+                    Notification.Create(NotificationType.Text, "Code de confirmation", 0, "Entrez le code recus sur votre mobile", force: true, completion: (value, text) =>
+                    {
+                        HTTP.AuthDeviceReply(username.text, text, (success_reply) =>
+                        {
+                            if (success_reply)
+                            {
+                                SaveLoad.save_user();
+                                FadingManager.I.Fade();
+                            }
+                            else
+                            {
+                                withoutPass.interactable = true;
+                                ColorBlock cb = connect.colors;
+                                cb.normalColor = new Color(229f / 255f, 76f / 255f, 76f / 255f);
+                                withoutPass.colors = cb;
+                            }
+                        });
+                    });
+                else
+                {
+                    withoutPass.interactable = true;
+                    ColorBlock cb = connect.colors;
+                    cb.normalColor = new Color(229f / 255f, 76f / 255f, 76f / 255f);
+                    withoutPass.colors = cb;
+                }
+            });
+        }
 
         public void LinkInscription()
         {
