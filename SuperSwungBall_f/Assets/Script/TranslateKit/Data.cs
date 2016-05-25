98fd8e1f13c2ddaedbe205d21dfd3a323af43929
hugo082 <hugo-boss12@hotmail.fr>
2016-05-25 21:23:39 +0200

    Translate
    Plus qu’a ajouter les données et les utiliser !

---
+++ SuperSwungBall_f/Assets/Script/TranslateKit/Data.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Boomlagoon.JSON;
+
+
+namespace TranslateKit {
+
+	public class Data {
+
+		public string standingTitle;
+
+
+		public Data(JSONObject obj){
+			this.standingTitle = obj.GetString ("standingTitle");
+		}
+
+	}
+
+}
---
+++ SuperSwungBall_f/Assets/Script/TranslateKit/Language.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Boomlagoon.JSON;
+
+namespace TranslateKit {
+	
+	public static class Language {
+
+		public static Data data;
+
+		public static void LoadLanguage(string uid){
+
+			string jsonString = ((TextAsset)Resources.Load("TranslateData", typeof(TextAsset))).text;
+			JSONObject json = JSONObject.Parse (jsonString);
+
+			if (json.ContainsKey (uid)) {
+				JSONObject languageObj = json.GetObject (uid);
+				data = new Data (languageObj);
+			}
+		}
+	}
+
+}
