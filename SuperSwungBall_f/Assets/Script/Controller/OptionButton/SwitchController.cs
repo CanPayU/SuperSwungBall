eed3a01367dcaa10826fdccc6aae5eb9f412ffc1
hugo082 <hugo-boss12@hotmail.fr>
2016-05-25 23:02:21 +0200

    Paramètres avancé
    Mise en place des paramètres avancé

---
+++ SuperSwungBall_f/Assets/Script/Controller/OptionButton/SwitchController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class SwitchController : MonoBehaviour {
+
+	private Slider slider;
+	private Image background;
+	private Image round;
+
+	void Awake(){
+		this.background = transform.Find ("Background").GetComponent<Image> ();
+		this.round = transform.Find ("Handle Slide Area").Find ("Handle").GetComponent<Image> ();
+		this.slider = GetComponent<Slider> ();
+		this.slider.onValueChanged.AddListener (delegate {OnValueChange ();});
+		setUpColor ();
+	}
+
+	private void OnValueChange(){
+		setUpColor ();
+	}
+	
+	private void setUpColor(){
+		if (slider.value <= 0) {
+			this.background.color = Colors.Normal.Red;
+			this.round.color = Colors.Pressed.Red;
+		} else {
+			this.background.color = Colors.Normal.Green;
+			this.round.color = Colors.Pressed.Green;
+		}
+		Debug.Log (slider.value);
+	}
+}
