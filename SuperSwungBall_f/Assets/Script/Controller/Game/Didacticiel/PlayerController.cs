eddff21c0fb85a5ddede34de69c4b5e3e078d9c2
Lombrix <romain.foucher@epita.fr>
2016-06-18 21:43:34 +0200

    Indentation + warning
    ctrl K+D dans les deux scripts
    réglage d'un warning dans l'instantiator controller (j'ai lassé en
    commentaire, "au cas ou" la version initiale

--- SuperSwungBall_f/Assets/Script/Controller/Game/Didacticiel/PlayerController.cs
+++ SuperSwungBall_f/Assets/Script/Controller/Game/Didacticiel/PlayerController.cs
@@ -8,7 +8,7 @@ namespace GameScene.Didacticiel
     public class PlayerController : BasicPlayerController
     {
 
-		public bool settablePointDeplacement = false;
+        public bool settablePointDeplacement = false;
 
         public PlayerController()
         {
@@ -19,29 +19,29 @@ namespace GameScene.Didacticiel
             get { return menuController; }
         }
 
-		protected virtual void Start()
-		{
-			this.Menu = Resources.Load("Prefabs/menu") as GameObject;
+        protected override void Start()
+        {
+            this.Menu = Resources.Load("Prefabs/menu") as GameObject;
 
-			//initialisation menu
-			Menu = Instantiate(Menu, new Vector3(), Quaternion.Euler(0, -90, 0)) as GameObject;
-			Menu.transform.parent = transform;
+            //initialisation menu
+            Menu = Instantiate(Menu, new Vector3(), Quaternion.Euler(0, -90, 0)) as GameObject;
+            Menu.transform.parent = transform;
 
-			menuController = Menu.GetComponent<MenuController>();
-			myCollisionController = GetComponent<CollisionController>();
-			selection = transform.FindChild("selection").GetComponent<Renderer>();
-			selection.enabled = false;
-			infoJoueur = GameObject.Find("Canvas").transform.FindChild("InfoJoueur").GetComponent<InfoJoueurController>();
-			flecheController = transform.FindChild("fleche").GetComponent<FlecheController>();
+            menuController = Menu.GetComponent<MenuController>();
+            myCollisionController = GetComponent<CollisionController>();
+            selection = transform.FindChild("selection").GetComponent<Renderer>();
+            selection.enabled = false;
+            infoJoueur = GameObject.Find("Canvas").transform.FindChild("InfoJoueur").GetComponent<InfoJoueurController>();
+            flecheController = transform.FindChild("fleche").GetComponent<FlecheController>();
 
-			Debug.Log ("start");
-			myCollider = GetComponent<Collider>();
-			limiteTerrain = GameObject.Find("terrain").transform.FindChild("limites").gameObject;
+            Debug.Log("start");
+            myCollider = GetComponent<Collider>();
+            limiteTerrain = GameObject.Find("terrain").transform.FindChild("limites").gameObject;
 
-			anim = transform.FindChild("perso").GetComponent<Animator>();
-			animationDeplacement = "Course";
-			phaseAnimation = false;
-			movement = false;
-		}
+            anim = transform.FindChild("perso").GetComponent<Animator>();
+            animationDeplacement = "Course";
+            phaseAnimation = false;
+            movement = false;
+        }
     }
 }
--- SuperSwungBall_f/Assets/Script/Controller/Game/Instantiator/InstantiatorController.cs
+++ SuperSwungBall_f/Assets/Script/Controller/Game/Instantiator/InstantiatorController.cs
@@ -2,54 +2,58 @@
 using System;
 
 
-namespace GameScene.Instantiator {
+namespace GameScene.Instantiator
+{
 
 
-	public class InstantiatorController : MonoBehaviour
-	{
-		[SerializeField]
-		private GameType type;
+    public class InstantiatorController : MonoBehaviour
+    {
+        [SerializeField]
+        private GameType type;
 
-		private Type solo;
-		private Type multi;
-		private Type replay;
+        private Type solo;
+        private Type multi;
+        private Type replay;
 
-		private bool instanciated = false;
+        private bool instanciated = false;
 
-//		private bool gameIsReplay = false;
-//		private bool gameIsSolo = false;
-//		private bool gameIsMulti = true;
+        //		private bool gameIsReplay = false;
+        //		private bool gameIsSolo = false;
+        //		private bool gameIsMulti = true;
 
-		// Use this for initialization
-		public void Awake () {
-			if (instanciated)
-				return;
+        // Use this for initialization
+        public void Awake()
+        {
+            if (instanciated)
+                return;
 
-			initialiseType();
+            initialiseType();
 
-			if (ApplicationModel.TypeToInstanciate != null)
-				instanciateMain (ApplicationModel.TypeToInstanciate);
-			else
-				instanciateMain (type);
+            //if (ApplicationModel.TypeToInstanciate != null)
+            instanciateMain(ApplicationModel.TypeToInstanciate);
+            //else
+            //	instanciateMain (type);
 
-		}
+        }
 
-		private void initialiseType(){
-			this.solo = typeof(GameScene.Solo.Main_Controller);
-			this.multi = typeof(GameScene.Multi.MainController);
-			this.replay = typeof(GameScene.Replay.MainController);
-		}
+        private void initialiseType()
+        {
+            this.solo = typeof(GameScene.Solo.Main_Controller);
+            this.multi = typeof(GameScene.Multi.MainController);
+            this.replay = typeof(GameScene.Replay.MainController);
+        }
 
-		private void instanciateMain(GameType type) {
-			if (type == GameType.Replay) 
-				gameObject.AddComponent (this.replay);
-			else if (type == GameType.Solo) 
-				gameObject.AddComponent (this.solo);
-			else if (type == GameType.Multi) 
-				gameObject.AddComponent (this.multi);
+        private void instanciateMain(GameType type)
+        {
+            if (type == GameType.Replay)
+                gameObject.AddComponent(this.replay);
+            else if (type == GameType.Solo)
+                gameObject.AddComponent(this.solo);
+            else if (type == GameType.Multi)
+                gameObject.AddComponent(this.multi);
 
-			this.instanciated = true;
-		}
+            this.instanciated = true;
+        }
 
-	}
+    }
 }
